a18bfc54f20a61757806da6a3a5ba61d15618281
soso123456 <53469327+soso123456@users.noreply.github.com>
2019-07-30 14:35:08 +0200

    Add files via upload

--- creationVM.ps1
+++ creationVM.ps1
@@ -1,22 +1,22 @@
 for  ($i = 1; $i -le 3; $i++){
 
 
-$vmname="shina"+$i
+$vmname="so"+$i
 $VMLocalAdminUser = $env:USERNAME
 $VMLocalAdminSecurePassword = ConvertTo-SecureString $cred -AsPlainText -Force
 $cred = New-Object System.Management.Automation.PSCredential ($VMLocalAdminUser, $VMLocalAdminSecurePassword)
-$sshPublicKey= cat C:\Users\utilisateur\.ssh\id_rsa.pub
+$sshPublicKey= cat $home\.ssh\id_rsa.pub
 
 $vmConfig= New-AzVm `
-    -ResourceGroupName "shina" `
-    -Name "hello" `
+    -ResourceGroupName "sofia" `
+    -Name $vmname `
     -OpenPorts 80,3389,22 `
     -Image UbuntuLTS `
     -Credential $cred
 
 
 
-Add-AzVMSshPublicKey -VM $vmConfig  -KeyData $sshPublicKey -Path "/home/utilisateur/.ssh/authorized_keys"
+Add-AzVMSshPublicKey -VM $vmConfig  -KeyData $sshPublicKey -Path "/home/$env:username/.ssh/authorized_keys"
 
  }
 
